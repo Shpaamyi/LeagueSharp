58c64259815465e2efc3a31957c64d86589d7d02
Shpaamyi <maxmax19931@gmail.com>
2015-08-17 01:30:15 -0400

    LeagueSharp
    Shit

---
+++ AutoSharp Fixed/Auto/HowlingAbyss/DecisionMaker.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using AutoSharp.Auto.SummonersRift;
+using AutoSharp.Utils;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace AutoSharp.Auto.HowlingAbyss
+{
+    internal static class DecisionMaker
+    {
+        private static int _lastUpdate = 0;
+        public static void OnUpdate(EventArgs args)
+        {
+            if (Environment.TickCount - _lastUpdate < 100) return;
+            _lastUpdate = Environment.TickCount;
+
+            var player = Heroes.Player;
+
+            if (Decisions.ImSoLonely())
+            {
+                return;
+            }
+
+            if (Program.Config.Item("autosharp.options.healup").GetValue<bool>() && Decisions.HealUp())
+            {
+                return;
+            }
+
+            if (player.UnderTurret(true) && Wizard.GetClosestEnemyTurret().CountNearbyAllyMinions(700) <= 3 && Wizard.GetClosestEnemyTurret().CountAlliesInRange(700) == 0)
+            {
+                Program.Orbwalker.ActiveMode = MyOrbwalker.OrbwalkingMode.Mixed;
+                player.IssueOrder(GameObjectOrder.MoveTo, player.Position.Extend(HeadQuarters.AllyHQ.Position.RandomizePosition(), 800));
+                return;
+            }
+
+            if (Heroes.Player.InFountain())
+            {
+                Shopping.Shop();
+                Wizard.AntiAfk();
+            }
+
+            if (Decisions.Farm())
+            {
+                return;
+            }
+            Decisions.Fight();
+
+            if (Program.Orbwalker.GetOrbwalkingPoint().IsZero ||
+                Program.Orbwalker.GetOrbwalkingPoint() == Game.CursorPos)
+            {
+                Decisions.ImSoLonely();
+            }
+        }
+    }
+}
---
+++ AutoSharp Fixed/Utils/Positioning.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LeagueSharp.Common;
+using SharpDX;
+
+namespace AutoSharp.Utils
+{
+    internal static class Positioning
+    {
+        internal static List<Vector3> ValidPossibleMoves;
+        internal static Vector3 RandomlyChosenMove;
+        internal static int LastUpdate;
+
+        internal static void OnUpdate(EventArgs args)
+        {
+            if (Environment.TickCount - LastUpdate < 250) return;
+            LastUpdate = Environment.TickCount;
+
+            ValidPossibleMoves = new List<Vector3>();
+
+            var farthestAlly = Heroes.AllyHeroes.OrderByDescending(h => h.Distance(HeadQuarters.AllyHQ)).FirstOrDefault();
+
+            ValidPossibleMoves.Add(farthestAlly.Position.RandomizePosition()); //initialize the vectorlist with a position known to exist,
+                                                                               //so it doesn't follow the mouse anymore
+
+            var team = Heroes.AllyHeroes.Where(h => !h.IsDead && h.Distance(farthestAlly) < 300).ToList();
+
+            var teamPoly = team.Select(hero => new Geometry.Circle(hero.Position.To2D(), 200).ToPolygon()).ToList();
+
+            foreach (var hp in teamPoly)
+            {
+                foreach (var point in hp.Points)
+                {
+                    var v3 = point.To3D();
+                    if (!point.IsWall() && Heroes.EnemyHeroes.Count(e => e.Distance(v3) < 300) < Heroes.AllyHeroes.Count(e=>e.Distance(v3) <= 300))
+                    {
+                        ValidPossibleMoves.Add(v3);
+                    }
+                }
+            }
+            RandomlyChosenMove = ValidPossibleMoves.OrderBy(v => v.Distance(HeadQuarters.AllyHQ.Position)).FirstOrDefault();
+        }
+    }
+}
